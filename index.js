f24216f6b7d47dcb8c7c4024c608fd4cca021fe0
sergiomata <serchmata@gmail.com>
2021-02-23 17:59:58 -0600

    fix: adding default value

--- index.js
+++ index.js
@@ -1,44 +1,52 @@
 const moment = require('moment-timezone')
 
-const getAwaitTime = (timezone) => {
+const getAwaitTime = (timezone, defaultValue) => {
 
   const WORKING_HOURS_WEEKDAY = '06:00:00'
   const WORKING_HOURS_WEEKEND = '09:00:00'
-  const fullDate = moment('2021-02-27T23:32:16').tz(timezone)
+  //const fullDate = moment('2021-02-27T23:32:16').tz(timezone)
   //const getFullDate = moment().tz('America/Mexico_City').locale('es')
-  // const fullDate = moment().tz(timezone)
-  console.log(moment())
-  const nameDay = fullDate.format('dddd')
+  const fullDate = moment('2021-02-26T09:19:22-06:00').tz(timezone)
+  //const fullDate = moment('2021-02-23T19:19:22-06:00').tz(timezone)
   const fullDateUTC = fullDate.utc()
+  const getHours = fullDateUTC.format('HH')
+  const nameDay = fullDateUTC.format('dddd')
+  console.log("moment: ",moment())
+  console.log("fullDate: ",fullDate)
   console.log("fullDateUTC: ",fullDateUTC)
-
-
-  console.log("fullDate: ", fullDate)
   console.log("nameDay: ", nameDay)
+  console.log('getHours: ',getHours)
 
-  let newDay = fullDateUTC.add(1, 'days')
-  console.log("newDay: ", newDay)
-  const getDate = newDay.format('YYYY-MM-DD')
-  console.log("getDate: ", getDate)
-  let newDate
+  if ((getHours >= 4 && getHours < 12 ) && (nameDay !== 'Saturday' && nameDay !== 'Sunday')||
+      (getHours >= 4 && getHours < 14 ) && (nameDay === 'Saturday' || nameDay === 'Sunday')
+      ){
+    let newDay = fullDateUTC.add(1, 'days')
+    console.log("newDay: ", newDay)
+    const getDate = newDay.format('YYYY-MM-DD')
+    console.log("getDate: ", getDate)
+    let newDate
 
-  if(nameDay == 'Friday' || nameDay == 'Saturday'){
-    newDate = moment(`${getDate}T${WORKING_HOURS_WEEKEND}`)
-    
-  }else {
-    console.log(`${getDate}T${WORKING_HOURS_WEEKDAY}`)
-    newDate = moment(`${getDate}T${WORKING_HOURS_WEEKDAY}`)
+    if(nameDay === 'Saturday' || nameDay === 'Sunday'){
+      newDate = moment(`${getDate}T${WORKING_HOURS_WEEKEND}`)
+    }else {
+      console.log(`${getDate}T${WORKING_HOURS_WEEKDAY}`)
+      newDate = moment(`${getDate}T${WORKING_HOURS_WEEKDAY}`)
+    }
+
+    console.log("newDate: ", newDate)
+    let differenceTime = newDate.diff(fullDateUTC)
+    console.log("differenceTime: ", differenceTime)
+    let duration = moment.duration(differenceTime)
+    let hours = duration.asHours()
+
+    waitTime = `${Math.ceil(hours)} hours`
+    console.log(waitTime)
+    return waitTime
+  }
+  else {
+    return defaultValue
   }
 
-  console.log("newDate: ", newDate)
-  let differenceTime = newDate.diff(fullDateUTC)
-  console.log("differenceTime: ", differenceTime)
-  let duration = moment.duration(differenceTime)
-  let hours = duration.asHours()
-
-  console.log(`${Math.ceil(hours)} hours`)
 }
 
-
-
-getAwaitTime('America/Bogota');
+console.log(getAwaitTime('America/Mexico_City','30 seconds'));
